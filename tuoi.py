b946d5052ea67fa0f1bc0aa5fea867deafec5eac
NguyenXuanAnhVy <anhvy12052005@gmail.com>
2024-08-21 20:14:06 +0700

    Add files via upload

---
+++ tuoi.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 21 20:00:10 2024
+
+@author: Student
+"""
+
+import math
+nam_sinh= int(input("Nhap nam sinh= "))
+nam_hiennay= int(input("Nhap nam hien nay= "))
+tuoi= nam_hiennay - nam_sinh
+print("Tuoi hien tai= ", tuoi)
