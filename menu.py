100c78e4bc99dc44e58f64a414599ea4b3f92f9f
NguyenXuanAnhVy <anhvy12052005@gmail.com>
2024-08-22 15:10:08 +0700

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 14:53:22 2024
+
+@author: anhvy
+"""
+print("========== MENU ==========")
+print("1.Hu tieu")
+print("2.Chao Long")
+print("3.Banh canh")
+print("4.Bun rieu")
+print("5.Pho bo")
+print("===========================")
+print(" Moi nhap lua chon: ")
+print("===========================")
+
