4c24f7a45c0fb9e294e6d56628c5641e9a657dad
NguyenXuanAnhVy <anhvy12052005@gmail.com>
2024-08-22 19:26:55 +0700

    Add files via upload

---
+++ chuoi1a.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 18:44:08 2024
+
+@author: Student
+"""
+
+str_name= ("Dai hoc Quoc gia, Khu pho 6, P.Linh Trung, Q.Thu Duc, Tp. HCM")
+print(str_name.replace(",", "\n"))
---
+++ chuoi1b.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 18:57:35 2024
+
+@author: Student
+"""
+
+str_name= ("Dai hoc Quoc gia, \nKhu pho 6, P.Linh Trung, Q.Thu Duc, Tp. HCM")
+print(str_name.replace(", P.", "\n").replace(", Q.", "\n").replace(", Tp.", "\n"))
